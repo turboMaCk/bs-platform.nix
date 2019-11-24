c20e8dc8703ad7975c99d76b5779d31c86078d98
Marek Fajkus <marek.faj@gmail.com>
2019-11-24 17:41:47 +0100

    updates

--- bs-platform.nix
+++ bs-platform.nix
@@ -26,10 +26,7 @@ stdenv.mkDerivation {
     sed -i 's:./configure.py --bootstrap:python3.5 ./configure.py --bootstrap:' ./scripts/install.js
 
     mkdir -p ./native/${ocaml-version}/bin
-    for name in $(find ${ocaml}/bin -printf "%P\n");
-    do
-        ln -sf ${ocaml}/bin/$name ./native/${ocaml-version}/bin/$name
-    done
+    ln -sf ${ocaml}/bin/*  ./native/${ocaml-version}/bin
 
     rm -f vendor/ninja/snapshot/ninja.linux
     cp ${ninja}/bin/ninja vendor/ninja/snapshot/ninja.linux
@@ -49,6 +46,7 @@ stdenv.mkDerivation {
     mkdir -p $out/bin
 
     cp -rf jscomp lib vendor odoc_gen native $out
+    cp bsconfig.json package.json $out
 
     ln -s $out/lib/bsb $out/bin/bsb
     ln -s $out/lib/bsc $out/bin/bsc
